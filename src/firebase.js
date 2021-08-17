36961a2497b1a0bfa83546e2adc7db34a1542403
Harsimran singh <simran.9971290436@gmail.com>
2021-08-17 16:19:33 +0530

    Reels App

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8QNahgtQ8NGXXjG8HfA1AoIgct_JqXRI",
+    authDomain: "reels-app-47341.firebaseapp.com",
+    projectId: "reels-app-47341",
+    storageBucket: "reels-app-47341.appspot.com",
+    messagingSenderId: "220959761959",
+    appId: "1:220959761959:web:eb451630da993dff739002"
+};
+
+firebase.initializeApp(firebaseConfig);
