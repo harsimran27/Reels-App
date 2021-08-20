1ba54d5e17712d43721c037528060fe6ab5a8105
Harsimran singh <simran.9971290436@gmail.com>
2021-08-21 01:01:31 +0530

    add video to UI

--- src/videCard.jsx
+++ src/videCard.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import "./videoCard.css";
 
 let VideoCard = () => {
+  let [playing, setPlaying] = useState(true);
+
   return (
     <div className="video-card">
+      <video
+        loop
+        onClick={(el) => {
+          if (playing) {
+            el.currentTarget.pause();
+            setPlaying(false);
+          } else {
+            el.currentTarget.play();
+            setPlaying(true);
+          }
+        }}
+        src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+        className="video-card-video"
+      ></video>
       <div className="video-card-username">some-user</div>
       <div className="video-card-music">
         <span class="material-icons-outlined">music_note</span>
