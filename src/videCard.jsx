a30f3d63d7d6505131841eb0c78593c8f39cd6cb
Harsimran singh <simran.9971290436@gmail.com>
2021-08-23 16:58:09 +0530

    create comment section ui

--- src/videCard.jsx
+++ src/videCard.jsx
@@ -20,8 +20,33 @@ let VideoCard = () => {
         src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
         className="video-card-video"
       ></video>
-      {commentBoxOpen ? <div className="video-card-comment-box"></div> : ""};
-      <div className="video-card-username">some-user</div>
+      {commentBoxOpen ? (
+        <div className="video-card-comment-box">
+          <div className="actual-comments">
+            <div className="post-user-comment">
+              <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+              <div>
+                <h5>user name</h5>
+                <p>This is actual comment</p>
+              </div>
+            </div>
+            <div className="post-user-comment">
+              <img src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
+              <div>
+                <h5>user name</h5>
+                <p>This is actual comment</p>
+              </div>
+            </div>
+          </div>
+          <div className="comment-form">
+            <input type="text" />
+            <button className="material-icons-outlined">send</button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+      ;<div className="video-card-username">some-user</div>
       <div className="video-card-music">
         <span className="material-icons-outlined">music_note</span>
         <marquee>some new song playing</marquee>
