edfcb3fa1f007c657841919d9939b862a4091dbb
Harsimran singh <simran.9971290436@gmail.com>
2021-08-23 16:49:03 +0530

    comment box

--- src/videCard.jsx
+++ src/videCard.jsx
@@ -3,7 +3,7 @@ import "./videoCard.css";
 
 let VideoCard = () => {
   let [playing, setPlaying] = useState(true);
-
+  let [commentBoxOpen, setCommentBoxOpen] = useState(false);
   return (
     <div className="video-card">
       <video
@@ -20,14 +20,25 @@ let VideoCard = () => {
         src="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
         className="video-card-video"
       ></video>
+      {commentBoxOpen ? <div className="video-card-comment-box"></div> : ""};
       <div className="video-card-username">some-user</div>
       <div className="video-card-music">
-        <span class="material-icons-outlined">music_note</span>
+        <span className="material-icons-outlined">music_note</span>
         <marquee>some new song playing</marquee>
       </div>
-
-      <span class="material-icons-outlined video-card-comment">chat</span>
-      <span class="material-icons-outlined video-card-like">
+      <span
+        onClick={() => {
+          if (commentBoxOpen) {
+            setCommentBoxOpen(false);
+          } else {
+            setCommentBoxOpen(true);
+          }
+        }}
+        className="material-icons-outlined video-card-comment"
+      >
+        chat
+      </span>
+      <span className="material-icons-outlined video-card-like">
         favorite_border
       </span>
     </div>
