94bd3ad445f9f845b6151991830978cd0eac0ba5
Harsimran singh <simran.9971290436@gmail.com>
2021-08-27 16:00:18 +0530

    save reels data in storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/app";
 import config from "./config";
 import "firebase/auth";
-
+import "firebase/storage";
 import "firebase/firestore";
 
 firebase.initializeApp(config);
@@ -9,7 +9,8 @@ firebase.initializeApp(config);
 let provider = new firebase.auth.GoogleAuthProvider();
 
 export const auth = firebase.auth();
-export const firestore = firebase.firestore()
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
 
 export const signInWithGoogle = () => {
 
